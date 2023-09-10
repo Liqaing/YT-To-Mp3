1ed4216bc3bb849e624d2d23848dc6f75ef296ae
Rithy <huotrithy01@gmail.com>
2023-09-10 21:16:32 +0700

    + what i've done:
        - Railway cannot finde static file, so i install whitenoise program by fallow in instruction on stack overflow
        - Fix Forbidden (Origin checking failed) on Railway
    
    + what to do:
        - same as last commits

--- YTToMp3/YTToMp3/settings.py
+++ YTToMp3/YTToMp3/settings.py
@@ -27,8 +27,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-CSRF_TRUSTED_ORIGINS = ['https://.mydomain.com', 'https://.127.0.0.1']
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -128,3 +126,5 @@ STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ['https://.mydomain.com', 'https://.127.0.0.1']
