b18d37a4a4e479a73fb49e0d0cbfea7e838aef7c
Rithy <huotrithy01@gmail.com>
2023-09-10 22:06:10 +0700

    + what i've done:     - I encounter alot of problem on railway, so i tried using vercel
    + what to do:
        - same as last commit

--- YTToMp3/YTToMp3/settings.py
+++ YTToMp3/YTToMp3/settings.py
@@ -124,5 +124,3 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-CSRF_TRUSTED_ORIGINS = ['https://yt-to-mp3.up.railway.app', 'https://.127.0.0.1']
