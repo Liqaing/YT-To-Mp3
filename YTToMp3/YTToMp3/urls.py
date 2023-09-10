a4ea78db56b684e127fcbf2162fa23e62f5dd8b1
Rithy <huotrithy01@gmail.com>
2023-09-10 22:28:24 +0700

    + what i've done:     - I encounter alot of problem on railway, so i tried using vercel
    + what to do:
        - same as last commit

--- YTToMp3/YTToMp3/urls.py
+++ YTToMp3/YTToMp3/urls.py
@@ -14,6 +14,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
+
 from django.contrib import admin
 from django.urls import path, include
 
@@ -21,3 +24,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('YTToMp3App.urls'))
 ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
