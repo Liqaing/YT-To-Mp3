1c3acb5bad4a0f6c89660e26fce7a12b5308583a
Rithy <huotrithy01@gmail.com>
2023-09-11 18:54:03 +0700

    + what i've done:     - improve regular expression that capture video id from     - deploy on vercel
    + what to do:
        - improve design of display info, nav bar (i will add search function)
        - add youtube api so user can search and download what they want without needing to copy url

--- YTToMp3/YTToMp3App/util.py
+++ YTToMp3/YTToMp3App/util.py
@@ -5,13 +5,17 @@ import requests
 # Retrive video id from URL
 def get_yt_video_id(url: str) -> str:
     
+    # Url sample
+    # https://www.youtube.com/watch?v=B3T50jjIvpg&list=RDMMtc2tW0jFHPo&index=5
+    # https://youtu.be/B3T50jjIvpg?si=nlQjeYHGKM-qwCxZ
+
     # Pattern of URL
-    video_url_pattern = r"^https://youtu.be/([a-zA-Z0-9_-]{11})"
+    video_url_pattern = r"^https://(www\.)?(youtube|youtu)?\.(com|be)/(watch\?v=)?([a-zA-Z0-9_-]{11})"
 
     # Check if url is matched with the pattern
     video_match = re.match(video_url_pattern, url)
     if video_match:
-        return video_match.group(1)
+        return video_match.group(5)
 
     return None
 
